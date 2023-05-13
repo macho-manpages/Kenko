9cad882e49346a2f7cf7418ef513470134e5949b
Chris <cmtrom01@louisville.edu>
2023-05-12 23:39:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kenko/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
